550b90e403aafefa204a2d781af6bf14b97b9e4d
Nagendra Nagarajayya <nnagarajayya@gmail.com>
2026-09-13 15:10:03 -0700

    Use scene translations for word reading

--- web/a/sw.js
+++ web/a/sw.js
@@ -1,6 +1,6 @@
 const SHARED_IMAGE = '/a/shared-image';
 const SHARED_CACHE = 'villagelens-shared-image-v1';
-const APP_VERSION = '2026-09-13.3';
+const APP_VERSION = '2026-09-13.4';
 
 self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting());
