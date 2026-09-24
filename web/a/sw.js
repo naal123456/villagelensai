448112841322d220be93dfa53bd23eba7b01ac28
Nagendra Nagarajayya <nnagarajayya@gmail.com>
2026-09-24 13:34:30 -0700

    Prepare persistent enrollment release

--- web/a/sw.js
+++ web/a/sw.js
@@ -1,6 +1,6 @@
 const SHARED_IMAGE = '/a/shared-image';
 const SHARED_CACHE = 'villagelens-shared-image-v1';
-const APP_VERSION = '2026-09-23.2';
+const APP_VERSION = '2026-09-24.1';
 
 self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting());
